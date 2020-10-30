17ad692282d24507a0381c4d4172097ec0ab228c
admin <alex.nester2477@gmail.com>
2020-10-30 20:07:55 +0300

    2 редакция

---
+++ src/entity/User.java
@@ -0,0 +1,21 @@
+package entity;
+
+public class User extends Person {
+    private String email;
+    private String password;
+
+    public User (String firstName, String lastName, String email, String password){
+        super(firstName, lastName);
+        this.email=email;
+        this.password=password;
+        }
+
+    public String getFullInfo (){
+        return ("Имя: "+getFullName()+"\n"+"E-mail: "+email);
+    }
+    public boolean isPasswordCorrect (String password) {
+        if (this.password.equals(password)) return true;
+        else return false;
+    }
+
+}
